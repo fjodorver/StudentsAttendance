bf4bc6ee8beaa1adaca6cea3858e8eb61db334f4
fjodor <fedor@vershinin.net>
2016-02-27 18:30:23 +0200

    Fixed locale

--- src/main/java/ee/ttu/vk/sa/pages/settings/LocalizationSettings.java
+++ src/main/java/ee/ttu/vk/sa/pages/settings/LocalizationSettings.java
@@ -3,6 +3,7 @@ package ee.ttu.vk.sa.pages.settings;
 import de.agilecoders.wicket.core.markup.html.bootstrap.form.BootstrapForm;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.Panel;
@@ -24,9 +25,9 @@ public class LocalizationSettings extends Panel {
     public LocalizationSettings(String id) {
         super(id);
         locale = this.getSession().getLocale();
-        List<Locale> locales = Arrays.asList(new Locale("en"), new Locale("ru"));
+        List<Locale> locales = Arrays.asList(new Locale("en", "US"), new Locale("ru", "RU"));
         form = new BootstrapForm<>("form", new CompoundPropertyModel<>(this));
-        form.add(new DropDownChoice<>("locale", locales));
+        form.add(new DropDownChoice<>("locale", locales, new ChoiceRenderer<>("displayLanguage")));
         form.add(new AjaxSubmitLink("save", form) {
             @Override
             protected void onSubmit(AjaxRequestTarget target, Form<?> ajaxForm) {
