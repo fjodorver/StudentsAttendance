3a442dfacc0565d48874c86228e92a9615170f66
vadimstrukov <vadim.strukov@gmail.com>
2016-03-02 21:57:54 +0200

    dashboard panel

---
+++ src/main/java/ee/ttu/vk/sa/pages/panels/DashboardPanel.java
@@ -0,0 +1,13 @@
+package ee.ttu.vk.sa.pages.panels;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ * Created by strukov on 3/2/16.
+ */
+public class DashboardPanel extends Panel {
+
+    public DashboardPanel(String id) {
+        super(id);
+    }
+}
