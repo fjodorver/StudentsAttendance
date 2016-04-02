6f7839e2a12c7e62afce3644c6305063ace896cb
vadimstrukov <vadim.strukov@gmail.com>
2016-04-02 17:06:49 +0300

    delete and sync timetable/attendance

--- src/main/java/ee/ttu/vk/attendance/service/AttendanceService.java
+++ src/main/java/ee/ttu/vk/attendance/service/AttendanceService.java
@@ -11,4 +11,5 @@ import java.util.List;
  */
 public interface AttendanceService extends ISaveService<Attendance>, IProviderService<Attendance, Attendance>, IFilterService<Attendance, Attendance> {
     void generateAndSaveAttendances(Programme programme);
+    void clearAll();
 }
